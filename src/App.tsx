9b59c0b9d13db2abc78315e10d86ef3c36baa8c5
nakashimashin <nakashimashin0517@gmail.com>
2024-01-20 01:18:42 +0900

    react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Auth from "./pages/Auth";
+import Game from "./pages/Game";
 
 function App() {
   return (
     <>
-      <div>Hello World</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Auth />} />
+          <Route path="/game" element={<Game />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
