ed55cb49d6d3e73ab2e99692669b165495e3754a
nakashimashin <nakashimashin0517@gmail.com>
2024-01-20 21:55:03 +0900

    ポケモンの名前を表示

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,12 +1,42 @@
 import { MyButton } from "../components/MyButton";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Game.css";
 
 export const Game = () => {
+  const [pokemonData, setPokemonData] = useState<any>([]);
   const [text, setText] = useState<string>("test test");
   const [typing, setTyping] = useState<boolean>(false);
   const [position, setPosition] = useState<number>(0);
   const [typo, setTypo] = useState<number[]>([]);
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      let res: any = await getPokemon(pokeApiUrl);
+      loadPokemon(res.results);
+    };
+    fetchPokemonData();
+  }, []);
+
+  const pokeApiUrl = "https://pokeapi.co/api/v2/pokemon/";
+
+  const getPokemon = (url: string) => {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    });
+  };
+
+  const loadPokemon = async (data: any) => {
+    let _pokemonData: any = await Promise.all(
+      data.map((pokemon: any) => {
+        let pokemonrecord = getPokemon(pokemon.url);
+        return pokemonrecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
 
   const typingToggle = () => {
     setTyping(!typing);
@@ -32,26 +62,33 @@ export const Game = () => {
   };
 
   return (
-    <div onKeyDown={handleKey} tabIndex={0}>
-      <div id="textbox">
-        {text.split("").map((char, index) => (
-          <span
-            key={index}
-            className={
-              index < position
-                ? "typed-letters"
-                : typo.includes(index)
-                  ? "typo"
-                  : index === position
-                    ? "current-letter"
-                    : "waiting-letters"
-            }
-          >
-            {char}
-          </span>
-        ))}
+    <div>
+      <div>
+        {pokemonData.map((pokemon: any, i: number) => {
+          return <p key={i}>{pokemon.name}</p>;
+        })}
       </div>
-      <MyButton onClick={typingToggle}>{typing ? "OFF" : "ON"}</MyButton>
+      <div onKeyDown={handleKey} tabIndex={0}>
+        <div id="textbox">
+          {text.split("").map((char, index) => (
+            <span
+              key={index}
+              className={
+                index < position
+                  ? "typed-letters"
+                  : typo.includes(index)
+                    ? "typo"
+                    : index === position
+                      ? "current-letter"
+                      : "waiting-letters"
+              }
+            >
+              {char}
+            </span>
+          ))}
+        </div>
+        <MyButton onClick={typingToggle}>{typing ? "OFF" : "ON"}</MyButton>
+      </div>
     </div>
   );
 };
