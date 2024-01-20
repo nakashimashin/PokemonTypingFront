b48eb1c40759bf55b467911806e81906992dec98
nakashimashin <nakashimashin0517@gmail.com>
2024-01-20 16:57:35 +0900

    キー入力追加

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,15 +1,30 @@
 import { MyButton } from "../components/MyButton";
-import { useState } from "react";
+import React, { useState } from "react";
+import "./Game.css";
 
 export const Game = () => {
+  const [text, setText] = useState<string>("test test");
   const [typing, setTyping] = useState<boolean>(false);
+  const [position, setPosition] = useState<number>(0);
 
   const typingToggle = () => {
     setTyping(typing ? false : true);
   };
 
+  const handleKey = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    if (typing) {
+      if (e.key === text[position]) {
+        setPosition(position + 1);
+      }
+    }
+  };
+
   return (
-    <div>
+    <div onKeyDown={(e) => handleKey(e)} tabIndex={0}>
+      <div>
+        <span className="typed-letters">{text.slice(0, position)}</span>
+        <span className="waiting-letters">{text.slice(position)}</span>
+      </div>
       <MyButton onClick={typingToggle}>{typing ? "OFF" : "ON"}</MyButton>
     </div>
   );
