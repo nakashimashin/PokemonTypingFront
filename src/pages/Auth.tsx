10aeccc6454802fd7c9eb7ec7544f85992d5559f
nakashimashin <nakashimashin0517@gmail.com>
2024-02-01 23:58:20 +0900

    Auth画面のスタイリング

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,3 +1,22 @@
+import "./Auth.css";
+
 export const Auth = () => {
-  return <div>Auth</div>;
+  return (
+    <div className="body">
+      <div className="form-container">
+        <form>
+          <label htmlFor="name">Name</label>
+          <input id="name" type="text" />
+          <label htmlFor="mail">Mail</label>
+          <input id="mail" type="email" />
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" />
+
+          <button className="login-button" type="submit">
+            送信
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
